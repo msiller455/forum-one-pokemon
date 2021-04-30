ef3b65183328b1638ffb5def9d15307c502b4a2f
Michael <msiller455@gmail.com>
2021-04-29 21:39:14 -0700

    adds fetch call on component mount and saves response to state

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react'
 import './App.css';
 
 function App() {
+  const [ allPokemon, setAllPokemon ] = useState([])
+
+
+  useEffect(() => {
+    fetch('https://pokeapi.co/api/v2/pokemon?limit=60')
+    .then(response => response.json())
+    .then(data => {
+      // The data response contains objects with another endpoint to fetch to for each individual pokemon.
+      // The Promise.all returns all the data fetched organized in an array
+      return Promise.all(
+        data.results.map(poke => {
+          return fetch(poke.url).then(res => res.json())
+        })
+      )
+    })
+    .then(data => setAllPokemon(data))
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      Forum One Pokemon App
     </div>
   );
 }
